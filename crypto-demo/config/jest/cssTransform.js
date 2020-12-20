0a316ffe9e908ced5abd3f83c715a7801279d3c9
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2020-12-20 20:16:33 +0000

    Img

--- crypto-demo/config/jest/cssTransform.js
+++ crypto-demo/config/jest/cssTransform.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// This is a custom Jest transformer turning style imports into empt
+// This is a custom Jest transformer turning style imports into empty objects.
+// http://facebook.github.io/jest/docs/en/webpack.html
+
+module.exports = {
+  process() {
+    return 'module.exports = {};';
+  },
+  getCacheKey() {
+    // 
