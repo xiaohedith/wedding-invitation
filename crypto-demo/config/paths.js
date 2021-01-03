e9304eff86bf6da74dfa029e1a6a35f3f554ff3f
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2021-01-03 22:12:40 +0000

    Développeur Java / Python (H/F)

--- crypto-demo/config/paths.js
+++ crypto-demo/config/paths.js
@@ -31,4 +31,8 @@ const getPublicUrl = appPackageJson =>
 // single-page apps that may serve index.html for nested URLs like /todos/42.
 // We can't use a relative path in HTML because we don't want to load something
 // like /todos/42/static/js/bundle.7289d.js. We have to know the root.
-function getServedPath(appP
+function getServedPath(appPackageJson) {
+  const publicUrl = getPublicUrl(appPackageJson);
+  const servedUrl =
+    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/');
+  return ensu
