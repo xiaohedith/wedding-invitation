b83eacefd19c54ebb94785290f1bd4026bdcc4af
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2021-01-17 20:14:48 +0000

    jbd2: Fix use after free after error in jbd2_journal_dirty_metadata()

--- crypto-demo/config/webpack.config.dev.js
+++ crypto-demo/config/webpack.config.dev.js
@@ -22,3 +22,9 @@ const publicPath = '/';
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
 const publicUrl = '';
+// Get environment variables to inject into our app.
+const env = getClientEnvironment(publicUrl);
+
+// style files regexes
+const cssRegex = /\.css$/;
+cons
