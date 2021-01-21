b477cea639766cb8c537dcf952661951040d2569
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2021-01-21 12:18:35 +0000

    version 0.4.6

--- crypto-demo/config/webpack.config.dev.js
+++ crypto-demo/config/webpack.config.dev.js
@@ -50,4 +50,12 @@ const getStyleLoaders = (cssOptions, preProcessor) => {
         ident: 'postcss',
         plugins: () => [
           require('postcss-flexbugs-fixes'),
-          
+          require('postcss-preset-env')({
+            autoprefixer: {
+              flexbox: 'no-2009',
+            },
+            stage: 3,
+          }),
+        ],
+      },
+    }
