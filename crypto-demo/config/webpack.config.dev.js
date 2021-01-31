159dd9c46d1c02bffb2f1255ed60d888fa0238b9
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2021-01-31 10:44:29 +0000

    fixed about

--- crypto-demo/config/webpack.config.dev.js
+++ crypto-demo/config/webpack.config.dev.js
@@ -83,4 +83,6 @@ module.exports = {
     // of CSS changes), or refresh the page (in case of JS changes). When you
     // make a syntax error, this client will display a syntax error overlay.
     // Note: instead of the default WebpackDevServer client, we use a custom one
-    // to bring better
+    // to bring better experience for Create React App users. You can replace
+    // the line below with these two lines if you prefer the stock client:
+    // require.resolve('webpack-de
