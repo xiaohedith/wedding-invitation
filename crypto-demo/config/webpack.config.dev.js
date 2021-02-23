57fdb11e25df1b19c11320b431c2a69b7b663ae0
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2021-02-23 09:47:23 +0000

    release 0.2.6

--- crypto-demo/config/webpack.config.dev.js
+++ crypto-demo/config/webpack.config.dev.js
@@ -111,4 +111,8 @@ module.exports = {
   },
   optimization: {
     // Automatically split vendor and commons
-    // https://twitter.com/wSokra/status/969633336732905
+    // https://twitter.com/wSokra/status/969633336732905474
+    // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
+    splitChunks: {
+      chunks: 'all',
+      na
