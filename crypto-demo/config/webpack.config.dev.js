e2c99b368bb85c1bf8624472043511a881806e8a
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2021-03-07 20:45:34 +0000

    Brian: restaurant update (Friday Apr 14)

--- crypto-demo/config/webpack.config.dev.js
+++ crypto-demo/config/webpack.config.dev.js
@@ -150,4 +150,10 @@ module.exports = {
       // This often causes confusion because we only process files within src/ with babel.
       // To fix this, we prevent you from importing files out of src/ -- if you'd like to,
       // please link the files into your node_modules/ and let module-resolution kick in.
-      // Make sure your source files are com
+      // Make sure your source files are compiled, as they will not be processed in any way.
+      new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
+    ],
+  },
+  resolveLoader: {
+    plugins: [
+      // Also r
