59707073a32e57e4c61464bcf8785d7dfd150e13
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2021-03-13 11:30:02 +0000

    v3.5.1

--- crypto-demo/config/webpack.config.dev.js
+++ crypto-demo/config/webpack.config.dev.js
@@ -156,4 +156,6 @@ module.exports = {
   },
   resolveLoader: {
     plugins: [
-      // Also r
+      // Also related to Plug'n'Play, but this time it tells Webpack to load its loaders
+      // from the current package.
+      PnpWebpackPlugin.moduleLoader(module),
