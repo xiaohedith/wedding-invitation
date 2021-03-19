13cac454eeb714ce65ffb9439b9e9600e32baa05
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2021-03-19 05:46:13 +0000

    update head

--- crypto-demo/config/webpack.config.dev.js
+++ crypto-demo/config/webpack.config.dev.js
@@ -176,4 +176,11 @@ module.exports = {
           {
             options: {
               formatter: require.resolve('react-dev-utils/eslintFormatter'),
-      
+              eslintPath: require.resolve('eslint'),
+
+            },
+            loader: require.resolve('eslint-loader'),
+          },
+        ],
+        include: paths.appSrc,
+     
