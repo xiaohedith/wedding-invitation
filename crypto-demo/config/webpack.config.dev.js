d63ddeca03195b0db66ccd921381041e101af7c8
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2021-03-23 18:56:21 +0000

    Added testing

--- crypto-demo/config/webpack.config.dev.js
+++ crypto-demo/config/webpack.config.dev.js
@@ -213,4 +213,8 @@ module.exports = {
           // Process application JS with Babel.
           // The preset includes JSX, Flow, and some ESnext features.
           {
-            test: /
+            test: /\.(js|jsx)$/,
+            include: paths.appSrc,
+            loader: require.resolve('babel-loader'),
+            options: {
+              customize
