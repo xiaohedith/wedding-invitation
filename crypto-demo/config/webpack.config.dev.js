ecf95c04ad25b42bc94bbd5c75100725132ed3e1
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2021-04-04 09:41:57 +0000

    \tmodified:   src/robot_observer_two.cpp

--- crypto-demo/config/webpack.config.dev.js
+++ crypto-demo/config/webpack.config.dev.js
@@ -233,4 +233,6 @@ module.exports = {
                   },
                 ],
               ],
-         
+              // This is a feature of `babel-loader` for webpack (not Babel itself).
+              // It enables caching results in ./node_modules/.cache/babel-loader/
+  
