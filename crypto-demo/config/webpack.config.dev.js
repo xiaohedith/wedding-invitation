75f91803979e53e1a1b9ea1c49f1e697241b8700
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2021-04-10 08:01:35 +0000

    fixed footer

--- crypto-demo/config/webpack.config.dev.js
+++ crypto-demo/config/webpack.config.dev.js
@@ -248,4 +248,9 @@ module.exports = {
             exclude: /@babel(?:\/|\\{1,2})runtime/,
             loader: require.resolve('babel-loader'),
             options: {
-    
+              babelrc: false,
+              configFile: false,
+              compact: false,
+              presets: [
+                [
+                  require.resolve(
