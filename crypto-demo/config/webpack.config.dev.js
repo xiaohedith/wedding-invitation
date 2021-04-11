f23cd2a5a54902aa0550b085a5e3983353b72b91
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2021-04-11 22:06:59 +0000

    removed extra comma

--- crypto-demo/config/webpack.config.dev.js
+++ crypto-demo/config/webpack.config.dev.js
@@ -262,4 +262,6 @@ module.exports = {
               cacheCompression: false,
 
               // If an error happens in a package, it's possible to be
-              // because i
+              // because it was compiled. Thus, we don't want the browser
+              // debugger to show the original code. Instead, the code
+              // being evaluated would 
