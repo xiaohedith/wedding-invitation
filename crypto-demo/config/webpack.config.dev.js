616c37820628880c145eab26abe5bfe30cb6999f
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2021-04-22 22:22:55 +0000

    [1] Bumped release.

--- crypto-demo/config/webpack.config.dev.js
+++ crypto-demo/config/webpack.config.dev.js
@@ -314,4 +314,6 @@ module.exports = {
               'sass-loader'
             ),
           },
-          // "file"
+          // "file" loader makes sure those assets get served by WebpackDevServer.
+          // When you `import` an asset, you get its (virtual) filename.
+          // In production,
