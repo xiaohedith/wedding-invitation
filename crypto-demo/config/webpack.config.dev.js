418a57ddce9164b5a3f0e985627789c634605c2a
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2021-04-27 05:34:13 +0000

    DMA: PL330: Fix potential NULL pointer dereference in pl330_submit_req()

--- crypto-demo/config/webpack.config.dev.js
+++ crypto-demo/config/webpack.config.dev.js
@@ -344,4 +344,7 @@ module.exports = {
     }),
     // Makes some environment variables available in index.html.
     // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
-    // <link rel="shortcut icon
+    // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
+    // In development, this will be an empty string.
+    new InterpolateHtmlPlugin(HtmlWebpackPlugin, env.raw),
+    // This
