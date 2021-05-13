314963948cbb6ec88d021a6f9dbd0da4f9e2c0cc
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2021-05-13 07:31:09 +0000

    more training

--- crypto-demo/config/webpack.config.dev.js
+++ crypto-demo/config/webpack.config.dev.js
@@ -380,4 +380,13 @@ module.exports = {
   ],
 
   // Some libraries import Node modules but don't use them in the browser.
-  // Tell Webpack to provide empty mocks for them so importing them wo
+  // Tell Webpack to provide empty mocks for them so importing them works.
+  node: {
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'mock',
+    tls: 'empty',
+    child_process: 'empty',
+    dns: 'mock'
+  },
+  // Turn off perform
