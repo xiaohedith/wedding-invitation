3c0a1456a50b0c465f0a6d35f0c393f0f4c1304c
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2021-05-23 11:18:01 +0000

    Bump version to 0.4

--- crypto-demo/config/webpackDevServer.config.js
+++ crypto-demo/config/webpackDevServer.config.js
@@ -3,4 +3,9 @@
 const errorOverlayMiddleware = require('react-dev-utils/errorOverlayMiddleware');
 const evalSourceMapMiddleware = require('react-dev-utils/evalSourceMapMiddleware');
 const noopServiceWorkerMiddleware = require('react-dev-utils/noopServiceWorkerMiddleware');
-const ignoredF
+const ignoredFiles = require('react-dev-utils/ignoredFiles');
+const config = require('./webpack.config.dev');
+const paths = require('./paths');
+const fs = require('fs');
+
+cons
