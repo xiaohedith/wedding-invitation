137b5ccec3211ccfeec5cd20aea330c2935467f2
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2021-05-23 18:50:15 +0000

    Remove console

--- crypto-demo/config/webpackDevServer.config.js
+++ crypto-demo/config/webpackDevServer.config.js
@@ -8,4 +8,9 @@ const config = require('./webpack.config.dev');
 const paths = require('./paths');
 const fs = require('fs');
 
-cons
+const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
+const host = process.env.HOST || '0.0.0.0';
+
+module.exports = function(proxy, allowedHost) {
+  return {
+    /
