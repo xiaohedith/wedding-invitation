684ac386cbd548ef4f92b8153651e42c95eac075
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2021-06-19 09:47:21 +0000

    Updated Footer

--- crypto-demo/config/webpackDevServer.config.js
+++ crypto-demo/config/webpackDevServer.config.js
@@ -45,4 +45,6 @@ module.exports = function(proxy, allowedHost) {
     // get served. Our build script will copy `public` into the `build` folder.
     // In `index.html`, you can get URL of `public` folder with %PUBLIC_URL%:
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
-    // In JavaScript code, you ca
+    // In JavaScript code, you can access it with `process.env.PUBLIC_URL`.
+    // Note that we only recommend to use `public` folder as an escape hatch
+    // for files like `favicon.ico`, `
