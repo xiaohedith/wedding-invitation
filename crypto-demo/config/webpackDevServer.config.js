f2571aa4b79f9edb8277fb0aa8e6c4d716a1c0cd
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2021-07-01 14:40:15 +0000

    Prepare 0.9.5 release

--- crypto-demo/config/webpackDevServer.config.js
+++ crypto-demo/config/webpackDevServer.config.js
@@ -82,3 +82,8 @@ module.exports = function(proxy, allowedHost) {
       disableDotRule: true,
     },
     public: allowedHost,
+    proxy,
+    before(app, server) {
+      if (fs.existsSync(paths.proxySetup)) {
+        // This registers user provided middleware for proxy reasons
+        
