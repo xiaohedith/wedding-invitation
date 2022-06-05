4a01b7d47eece4fdf8d239bdfda926da4115b53e
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2022-06-05 10:30:03 +0000

    fixed pom

--- crypto-demo/scripts/build.js
+++ crypto-demo/scripts/build.js
@@ -1,4 +1,8 @@
 'use strict';
 
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_
+process.env.BABEL_ENV = 'production';
+process.env.NODE_ENV = 'production';
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future
