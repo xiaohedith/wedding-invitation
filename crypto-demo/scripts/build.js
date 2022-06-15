65464b6542b00545e6f58aff31e52deb70eb279d
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2022-06-15 22:51:22 +0000

    rename

--- crypto-demo/scripts/build.js
+++ crypto-demo/scripts/build.js
@@ -59,4 +59,9 @@ checkBrowsers(paths.appPath, isInteractive)
   })
   .then(previousFileSizes => {
     // Remove all content but keep the directory so that
-    // if you're in it, y
+    // if you're in it, you don't end up in Trash
+    fs.emptyDirSync(paths.appBuild);
+    // Merge with the public folder
+    copyPublicFolder();
+    // Start the webpack build
+    retur
