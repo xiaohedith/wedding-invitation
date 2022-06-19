e98668f6f074df3dbae38b65676fcbd1d19a8be1
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2022-06-19 23:11:39 +0000

    usb: musb: core: print new line in the driver banner again

--- crypto-demo/scripts/build.js
+++ crypto-demo/scripts/build.js
@@ -96,4 +96,7 @@ checkBrowsers(paths.appPath, isInteractive)
       console.log();
 
       const appPackage = require(paths.appPackageJson);
-      const publicUrl = paths
+      const publicUrl = paths.publicUrl;
+      const publicPath = config.output.publicPath;
+      const buildFolder = path.relative(process.cwd(), paths.appBuild);
+      printHostin
