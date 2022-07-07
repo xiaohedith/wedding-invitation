1992936c4a146d78121a06075909dc64f419bb2f
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2022-07-07 10:46:26 +0000

    New code

--- crypto-demo/scripts/test.js
+++ crypto-demo/scripts/test.js
@@ -6,4 +6,7 @@ process.env.NODE_ENV = 'test';
 process.env.PUBLIC_URL = '';
 
 // Makes the script crash on unhandled rejections instead of silently
-// ignoring them. In 
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+  throw 
