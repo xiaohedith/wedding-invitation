34be36f1be0629ae23e1c45cb0634cbe9ca0cddf
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2022-09-27 18:48:25 +0000

    add snapshots

--- crypto-demo/src/serviceWorker.js
+++ crypto-demo/src/serviceWorker.js
@@ -19,4 +19,6 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
-  if (process.env.NODE_ENV === 'production' && 'serviceWor
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    // The URL constructor is available in all browsers that support SW.
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.locat
