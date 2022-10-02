0d11dc7a50b47126c3056af3dd1792515f5a20c6
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2022-10-02 12:49:34 +0000

    Fix properties

--- crypto-demo/src/serviceWorker.js
+++ crypto-demo/src/serviceWorker.js
@@ -24,4 +24,9 @@ export function register(config) {
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
-      // from what our page i
+      // from what our page is served on. This might happen if a CDN is used to
+      // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+      return;
+    }
+
+    window.addEventListene
