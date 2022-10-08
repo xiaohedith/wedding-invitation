96807c47e28c2e707eb2708a8a6f7806d31785c3
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2022-10-08 11:12:46 +0000

    changed copyright

--- crypto-demo/src/serviceWorker.js
+++ crypto-demo/src/serviceWorker.js
@@ -33,4 +33,8 @@ export function register(config) {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
-        // This is running on localhost. Let's check if a service
+        // This is running on localhost. Let's check if a service worker still exists or not.
+        checkValidServiceWorker(swUrl, config);
+
+        // Add some additional logging to localhost, pointing developers to the
+   
