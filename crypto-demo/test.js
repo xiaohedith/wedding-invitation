12172c96281284284ee8fc5601a8c02d61119693
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2022-11-14 11:14:00 +0000

    improve debug

--- crypto-demo/test.js
+++ crypto-demo/test.js
@@ -1,4 +1,9 @@
 const log4js = require('log4js');
 
 function producer(){
-cons
+const logger = log4js.getLogger('producer');
+logger.trace('Entering cheese testing');
+
+  log4js.configure({
+        appenders: { 
+            producer: { type: 'file
