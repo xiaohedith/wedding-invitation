8703dc0f7019a13f1c7e68ebdc2224aeb8b62f32
xiaohedith <109772027+xiaohedith@users.noreply.github.com>
2022-11-14 19:34:14 +0000

    HAIL KING HARAMBE! #NeverForgetHarambe

--- crypto-demo/test.js
+++ crypto-demo/test.js
@@ -6,4 +6,10 @@ logger.trace('Entering cheese testing');
 
   log4js.configure({
         appenders: { 
-            producer: { type: 'file
+            producer: { type: 'file', filename: 'crypto-trader-producer.log', maxLogSize: 20 }
+        },
+        categories: { default: { appenders: ['producer'], level: 'info' } }
+        });
+}
+
+func
